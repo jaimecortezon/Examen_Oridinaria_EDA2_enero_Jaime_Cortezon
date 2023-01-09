207676e3e62bb3f060a2d6cd1f56a69b14f0116a
jaimecortezon <jaime.cortezon1@usp.ceu.es>
2023-01-09 16:15:18 +0100

    update pregunta2

---
+++ Pregunta2.py
@@ -0,0 +1,26 @@
+## Crea una clase llamada stormtrooper.py que tenga los atributos nombre y rango
+## Crea el constructor de la clase. Añadir en el constructor un print para informar de que el stormtrooper se ha creado con éxito.
+
+class Stormtrooper:
+    def __init__(self, nombre, rango):
+        self.nombre = nombre
+        self.rango = rango
+        print(f"Stormtrooper {self.nombre} de rango {self.rango} creado con éxito")
+
+## Crea una lista con un numero arbitrario de objetos tipo Stormtropper.
+troopers = [Stormtrooper("Finn", "Cabo"), Stormtrooper("Phasma", "Capitán"), Stormtrooper("Rey", "Cabo")]   
+
+## Recorre los elementos de la lista, y prueba a ejecutar el método calificación de cada objeto que has creado.
+class Stormtrooper:
+    def __init__(self, nombre, rango):
+        self.nombre = nombre
+        self.rango = rango
+        print(f"Stormtrooper {self.nombre} de rango {self.rango} creado con éxito")
+    
+    def calificación(self):
+        if self.rango == "Capitán":
+            return "Excelente"
+        elif self.rango == "Coronel":
+            return "Bueno"
+        else:
+            return "Regular"
